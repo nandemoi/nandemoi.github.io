a7ce39acac63906babbb0bea3807f823eb790b0d
nandemoi <nandemouda@gmail.com>
2023-05-20 12:23:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,92 +2,49 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import re
+import re, getpass
 
 app = Flask(__name__)
 
-app.secret_key = 'whatfor'
+app.secret_key = 'eltonhuang'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = input ('mysql root password: ')
+app.config['MYSQL_PASSWORD'] = getpass.getpass ('mysql password: ')
 app.config['MYSQL_DB'] = 'test'
 
 mysql = MySQL(app)
+
+logins = {}
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	msg = ''
 	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-		username = request.form['username']
-		password = request.form['password']
+		id = request.form['username']
+		pw = request.form['password']
 		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 		cursor.execute(
-			'SELECT * FROM accounts WHERE username = % s \
-			AND password = % s', (username, password, ))
+			'SELECT * FROM accounts WHERE id = % s \
+			AND pw = % s', (id, pw, ))
 		account = cursor.fetchone()
 		if account:
 			session['loggedin'] = True
 			session['id'] = account['id']
-			session['username'] = account['username']
-			msg = 'Logged in successfully !'
+			session['username'] = account['name']
+			msg = f"{account['name']}，今天繼續加油喔！"
 			return render_template('index.html', msg=msg)
 		else:
 			msg = 'Incorrect username / password !'
 	return render_template('login.html', msg=msg)
 
-
 @app.route('/logout')
 def logout():
-
-
-session.pop('loggedin', None)
-session.pop('id', None)
-session.pop('username', None)
-return redirect(url_for('login'))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-	msg = ''
-	if request.method == 'POST' and 'username' in
-	request.form and 'password' in request.form and
-	'email' in request.form and 'address' in
-	request.form and 'city' in request.form and
-	'country' in request.form and 'postalcode'
-	in request.form and 'organisation' in request.form:
-		username = request.form['username']
-		password = request.form['password']
-		email = request.form['email']
-		organisation = request.form['organisation']
-		address = request.form['address']
-		city = request.form['city']
-		state = request.form['state']
-		country = request.form['country']
-		postalcode = request.form['postalcode']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute(
-			'SELECT * FROM accounts WHERE username = % s', (username, ))
-		account = cursor.fetchone()
-		if account:
-			msg = 'Account already exists !'
-		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-			msg = 'Invalid email address !'
-		elif not re.match(r'[A-Za-z0-9]+', username):
-			msg = 'name must contain only characters and numbers !'
-		else:
-			cursor.execute('INSERT INTO accounts VALUES \
-			(NULL, % s, % s, % s, % s, % s, % s, % s, % s, % s)',
-						(username, password, email,
-							organisation, address, city,
-							state, country, postalcode, ))
-			mysql.connection.commit()
-			msg = 'You have successfully registered !'
-	elif request.method == 'POST':
-		msg = 'Please fill out the form !'
-	return render_template('register.html', msg=msg)
-
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 @app.route("/index")
 def index():
@@ -111,11 +68,7 @@ def display():
 def update():
 	msg = ''
 	if 'loggedin' in session:
-		if request.method == 'POST' and 'username' in request.form
-		and 'password' in request.form and 'email' in request.form and
-		'address' in request.form and 'city' in request.form and 'country'
-		in request.form and 'postalcode' in request.form and
-		'organisation' in request.form:
+		if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'address' in request.form and 'city' in request.form and 'country' in request.form and 'postalcode' in request.form and 'organisation' in request.form:
 			username = request.form['username']
 			password = request.form['password']
 			email = request.form['email']
